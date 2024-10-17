bf246deee67c13d9193e6828573593835dbee974
Rodrigo Almeida <c.almeidarodrigo@gmail.com>
2024-10-17 01:24:30 -0300

    added new functionalities to the views

--- src/courses/controllers/rating_controller.py
+++ src/courses/controllers/rating_controller.py
@@ -1,14 +1,24 @@
 from rest_framework import generics
+from rest_framework.generics import get_object_or_404
 
 from src.courses.models import Rating
 from src.courses.serializers import RatingSerializer
+
+from django.db.models import QuerySet
 
 class RatingBaseView:
     queryset = Rating.objects.all()
     serializer_class = RatingSerializer
 
 class RatingsAPIView(RatingBaseView, generics.ListCreateAPIView):
-    pass
+    def get_queryset(self) -> QuerySet[Rating]:
+        if not (course_pk := self.kwargs.get('course_pk')): return super().get_queryset()
+        
+        return self.queryset.filter(course_id=course_pk)
 
 class RatingAPIView(RatingBaseView, generics.RetrieveDestroyAPIView):
-    pass
+    def get_object(self) -> Rating:
+        if not (course_pk := self.kwargs.get('course_pk')): 
+            return get_object_or_404(self.get_queryset(), pk=self.kwargs.get('ratings_pk'))
+
+        return get_object_or_404(self.get_queryset(), course_id=course_pk, pk=self.kwargs.get('ratings_pk'))
--- src/courses/urls.py
+++ src/courses/urls.py
@@ -3,8 +3,13 @@ from django.urls import path
 from src.courses.controllers import RatingAPIView, RatingsAPIView, CourseAPIView, CoursesAPIView
 
 urlpatterns = [
-    path("courses/<int:pk>/", CourseAPIView.as_view(), name="courses"),
     path("courses/", CoursesAPIView.as_view(), name="courses"),
-    path("ratings/<int:pk>/", RatingAPIView.as_view(), name="ratings"),
+    path("courses/<int:pk>/", CourseAPIView.as_view(), name="courses"),
+
+    path("courses/<int:course_pk>/ratings", RatingsAPIView.as_view(), name="course_ratings"),
+    path("courses/<int:course_pk>/ratings/<int:ratings_pk>", RatingAPIView.as_view(), name="course_ratings"),
+
+
     path("ratings/", RatingsAPIView.as_view(), name="ratings"),
+    path("ratings/<int:ratings_pk>/", RatingAPIView.as_view(), name="ratings"),
 ]
