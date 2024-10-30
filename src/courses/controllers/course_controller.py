d3941a5dabbc67d19e016721d7132f103ab6ac2f
Rodrigo Almeida <c.almeidarodrigo@gmail.com>
2024-10-30 13:42:00 -0300

    started adding test, get test done

--- src/courses/controllers/course_controller.py
+++ src/courses/controllers/course_controller.py
@@ -30,7 +30,7 @@ class CourseAPIView(BaseCourseView, generics.RetrieveDestroyAPIView):
 API V2
 """
 class CoursesViewSet(BaseCourseView, viewsets.ModelViewSet):
-    permission_classes = (IsSuperUser,)
+    # permission_classes = (IsSuperUser,)
 
     @action(detail=True, methods=["get"])
     def ratings(self, request, pk=None):
---
+++ src/courses/test/get_tests.py
@@ -0,0 +1,10 @@
+import requests
+
+headers = {"authorization": "Token 264c186949728895f5759937e3416f7ac79d2958"}
+
+base_url_courses = "http://localhost:8000/api/v2/courses/"
+base_url_courses = "http://localhost:8000/api/v2/ratings/"
+
+result = requests.get(url=base_url_courses, headers=headers)
+
+assert result.status_code == 200
